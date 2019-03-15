60203f4c9997536ea3b090fa03e3c9ea2a7bd125
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-16 00:58:43 +0300

    add error returns in scope query

--- app/query/scopes.go
+++ app/query/scopes.go
@@ -10,16 +10,20 @@ import (
 )
 
 //GetScopes returns scopes for provided token.
-func GetScopes(req *http.Request) []string {
+func GetScopes(req *http.Request)([]string, error) {
 	accessToken, err := cookie.GetCookieValue(req, "access_token")
 
 	if err != nil {
-		log.Println(err.Error())
+		return nil, err
 	}
 
 	resp, err := http.Get("https://api.github.com/user?access_token=" + accessToken)
 
+	if err != nil {
+		return nil, err
+	}
+
 	respHeader := header.ReadResponseHeader(resp)
 
-	return strings.Split(respHeader["X-Oauth-Scopes"][0], ",")
+	return strings.Split(respHeader["X-Oauth-Scopes"][0], ","), nil
 }
