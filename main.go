eadcae1d18c0ff3ebcfc65b2cdc1f398b3ea504a
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-15 20:04:29 +0300

    add entry point

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+
+import (
+	"fmt"
+	"flag"
+	"log"
+	"net/http"
+	"github-aggregator-web/app/view/index"
+	"github-aggregator-web/app/api"
+)
+
+var host string
+var port string
+
+func init(){
+	flag.StringVar(&host, "host", "127.0.0.1", "")
+	flag.StringVar(&host, "h", "127.0.0.1", "")
+	flag.StringVar(&port, "port", "8080", "")
+	flag.StringVar(&port, "p", "8080", "")
+	flag.Parse()
+}
+
+func main(){
+	log.Printf("Server started on %s:%s",host,port)
+	http.HandleFunc("/",index.Render)
+	http.HandleFunc("/auth",api.Auth)
+	http.HandleFunc("/logout",api.Logout)
+	http.ListenAndServe(fmt.Sprintf("%s:%s",host,port), nil)
+}
