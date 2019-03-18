6b08522d02695cd1b9edf11d72325c8cf926b7b5
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-18 22:19:47 +0300

    ref scopes query

--- pkg/query/scopes.go
+++ pkg/query/scopes.go
@@ -9,7 +9,7 @@ import (
 	"github.com/777777miSSU7777777/github-aggregator/pkg/constants"
 )
 
-func GetScopes(tkn string)([]string, error) {
+func QueryScopes(tkn string)([]string, error) {
 	resp, err := http.Get( fmt.Sprintf("%s%s?%s%s", constants.GHApiURL, constants.User, constants.AccessTokenParam, tkn))
 
 	if err != nil {
