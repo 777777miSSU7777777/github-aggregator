cee197a67418e0b57c12161a14370955c061cd71
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-18 21:57:05 +0300

    ref user query

---
+++ pkg/query/user.go
@@ -0,0 +1,27 @@
+package query
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+
+	"github.com/777777miSSU7777777/github-aggregator/pkg/constants"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/http/bodyutil"
+)
+
+
+func QueryUser(req *http.Request, tkn string)([]byte, error){
+	resp, err := http.Get( fmt.Sprintf("%s%s?%s%s",constants.GHApiURL, constants.User, constants.AccessTokenParam, tkn) )
+
+	if err != nil {
+		return nil , err
+	}
+
+	userBody, err := bodyutil.ReadResponseBody(resp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userBody, nil
+}
