f3bb00addf702117b0b0471a24350388dd4ac6e4
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-19 20:27:28 +0300

    add factory for user entity

---
+++ pkg/factory/userfactory/user_factory.go
@@ -0,0 +1,19 @@
+package userfactory
+
+import (
+	"encoding/json"
+
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+)
+
+func New(userBytes []byte)(*entity.User, error){
+	user := entity.User{}
+
+	err := json.Unmarshal(userBytes, user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+} 
