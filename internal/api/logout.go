195d50250ea9337139e13956936c754f05ecd651
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-26 03:33:04 +0300

    built-in logging replaced with json logging

--- internal/api/logout.go
+++ internal/api/logout.go
@@ -1,20 +1,20 @@
 package api
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/777777miSSU7777777/github-aggregator/internal/security/webtokenservice"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/log"
 )
 
 // Logout logs out user from app.
 func Logout(rw http.ResponseWriter, req *http.Request) {
 	err := webtokenservice.DeleteToken(rw, req)
 	if err != nil {
-		log.Println(err)
+		log.Error.Println(err)
 	}
 
-	log.Println("Logout")
+	log.Info.Println("Logout")
 
 	http.Redirect(rw, req, "/", 301)
 }
