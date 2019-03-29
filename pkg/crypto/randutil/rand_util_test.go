bf07b24adde74b7d227a599274bdb2ae27d89129
777777miSSU7777777 <6x7missu7x7@gmail.com>
2019-03-29 19:12:02 +0300

    add tests for randutil

---
+++ pkg/crypto/randutil/rand_util_test.go
@@ -0,0 +1,59 @@
+package randutil
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomBytes__Size16__Successful(t *testing.T) {
+	randomBytes, _ := GenerateRandomBytes(16)
+
+	assert.Equal(t, 16, len(randomBytes))
+}
+
+func TestGenerateRandomBytes__Size16__Filled(t *testing.T) {
+	randomBytes, _ := GenerateRandomBytes(16)
+
+	zeroBytes := []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+
+	assert.NotEqual(t, zeroBytes, randomBytes)
+}
+
+func TestGenerateRandomBytes__Size0__Successful(t *testing.T) {
+	randomBytes, _ := GenerateRandomBytes(0)
+
+	assert.Equal(t, 0, len(randomBytes))
+}
+
+func TestGenerateRandomBytes__Size0__Empty(t *testing.T) {
+	randomBytes, _ := GenerateRandomBytes(0)
+
+	emptyByteArray := []byte{}
+
+	assert.Equal(t, emptyByteArray, randomBytes)
+}
+
+func TestGenerateRandomBytes__NegativeSize__Panics(t *testing.T) {
+	assert.Panics(t, func() {
+		GenerateRandomBytes(-16)
+
+	})
+}
+
+func TestGenerateRandomBytes__MockerRead__Error(t *testing.T) {
+	original := readFunc
+
+	defer func() {
+		readFunc = original
+	}()
+
+	readFunc = func(b []byte) (int, error) {
+		return 0, errors.New("Error")
+	}
+
+	_, err := GenerateRandomBytes(16)
+
+	assert.Error(t, err)
+}
