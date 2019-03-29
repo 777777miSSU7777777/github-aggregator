8b54fbd0b090f302ff7466cf123348a44ce4172e
777777miSSU7777777 <6x7missu7x7@gmail.com>
2019-03-29 19:24:05 +0300

    fmt tests for cryptofactory

--- pkg/factory/cryptofactory/crypto_factory_test.go
+++ pkg/factory/cryptofactory/crypto_factory_test.go
@@ -3,18 +3,18 @@ package cryptofactory
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/encryption/aes"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNew__aes_String__AES(t *testing.T){
+func TestNew__aes_String__AES(t *testing.T) {
 	testCryptoService := New("aes")
 	aesInstance := aes.AES{}
 
 	assert.ObjectsAreEqual(aesInstance, testCryptoService)
 }
 
-func TestNew__Default__AES(t *testing.T){
+func TestNew__Default__AES(t *testing.T) {
 	testCryptoService := New("123")
 	aesInstance := aes.AES{}
 
