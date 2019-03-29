4fac5af376201a657b02b1b4bc0c523c6f5d3e83
777777miSSU7777777 <6x7missu7x7@gmail.com>
2019-03-29 19:21:22 +0300

    add tests for cryptofactory

---
+++ pkg/factory/cryptofactory/crypto_factory_test.go
@@ -0,0 +1,22 @@
+package cryptofactory
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/encryption/aes"
+)
+
+func TestNew__aes_String__AES(t *testing.T){
+	testCryptoService := New("aes")
+	aesInstance := aes.AES{}
+
+	assert.ObjectsAreEqual(aesInstance, testCryptoService)
+}
+
+func TestNew__Default__AES(t *testing.T){
+	testCryptoService := New("123")
+	aesInstance := aes.AES{}
+
+	assert.ObjectsAreEqual(aesInstance, testCryptoService)
+}
