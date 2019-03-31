ce298f7156a3aebcfac17f846a6be20e79b6b462
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-03-31 16:04:36 +0300

    move http.Get and bodutil.ReadResponseBody funcs to variables

--- pkg/query/user.go
+++ pkg/query/user.go
@@ -4,10 +4,8 @@ package query
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/777777miSSU7777777/github-aggregator/pkg/constants"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/http/bodyutil"
 )
 
 // GetUser returns body of request to "https://api.github.com/user" for provided Github API access token.
@@ -15,13 +13,13 @@ import (
 // Body is presented as byte array.
 // If http.Get or bodyutil.ReadResponseBody occurs any error, this will be returned.
 func GetUser(tkn string) ([]byte, error) {
-	resp, err := http.Get(fmt.Sprintf("%s%s?%s%s", constants.GHApiURL, constants.User, constants.AccessTokenParam, tkn))
+	resp, err := httpGet(fmt.Sprintf("%s%s?%s%s", constants.GHApiURL, constants.User, constants.AccessTokenParam, tkn))
 
 	if err != nil {
 		return nil, err
 	}
 
-	userBody, err := bodyutil.ReadResponseBody(resp)
+	userBody, err := readResponseBody(resp)
 
 	if err != nil {
 		return nil, err
