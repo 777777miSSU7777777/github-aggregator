54e640f5b572e91b54d7c48490601fe291d36a71
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-03 00:39:54 +0300

    move imported funcs to variables

--- internal/security/webtokenservice/web_token_service.go
+++ internal/security/webtokenservice/web_token_service.go
@@ -12,6 +12,20 @@ import (
 	"github.com/777777miSSU7777777/github-aggregator/pkg/http/cookieutil"
 )
 
+var encode = base64util.Encode
+
+var decode = base64util.Decode
+
+var encrypt = cryptoservice.CryptoService.Encrypt
+
+var decrypt = cryptoservice.CryptoService.Decrypt
+
+var saveCookie = cookieutil.SaveCookie
+
+var getCookieValue = cookieutil.GetCookieValue
+
+var deleteCookie = cookieutil.DeleteCookie
+
 var cryptoService cryptoservice.CryptoService
 
 // SetCryptoService sets crypto service for web token service.
@@ -37,19 +51,19 @@ func SetCryptoServiceIV(IV []byte) {
 // If base64util.Decode or cryptoService.Encrypt or base64util.Encode
 // occurs any error, this will be returned.
 func SaveToken(rw http.ResponseWriter, tkn string) error {
-	decodedTkn, err := base64util.Decode(tkn)
+	decodedTkn, err := decode(tkn)
 	if err != nil {
 		return err
 	}
 
-	encryptedTkn, err := cryptoService.Encrypt(decodedTkn)
+	encryptedTkn, err := encrypt(cryptoService, decodedTkn)
 	if err != nil {
 		return err
 	}
 
-	encodedTkn := base64util.Encode(encryptedTkn)
+	encodedTkn := encode(encryptedTkn)
 
-	cookieutil.SaveCookie(rw, constants.AccessToken, encodedTkn)
+	saveCookie(rw, constants.AccessToken, encodedTkn)
 
 	return nil
 }
@@ -59,22 +73,22 @@ func SaveToken(rw http.ResponseWriter, tkn string) error {
 // If cookieutil.GetCookieValue or base64util.Decode or cryptoService.Decrypt
 // or base64util.Encode occurs any error, this will be returned.
 func GetToken(req *http.Request) (string, error) {
-	encodedTkn, err := cookieutil.GetCookieValue(req, constants.AccessToken)
+	encodedTkn, err := getCookieValue(req, constants.AccessToken)
 	if err != nil {
 		return "", err
 	}
 
-	decodedTkn, err := base64util.Decode(encodedTkn)
+	decodedTkn, err := decode(encodedTkn)
 	if err != nil {
 		return "", err
 	}
 
-	decryptedTkn, err := cryptoService.Decrypt(decodedTkn)
+	decryptedTkn, err := decrypt(cryptoService, decodedTkn)
 	if err != nil {
 		return "", err
 	}
 
-	tkn := base64util.Encode(decryptedTkn)
+	tkn := encode(decryptedTkn)
 
 	return tkn, nil
 }
@@ -82,7 +96,7 @@ func GetToken(req *http.Request) (string, error) {
 // DeleteToken deletes access token from cookie.
 // If cookieutil.DeleteCookie occurs any error, this will be returned.
 func DeleteToken(rw http.ResponseWriter, req *http.Request) error {
-	err := cookieutil.DeleteCookie(rw, req, constants.AccessToken)
+	err := deleteCookie(rw, req, constants.AccessToken)
 	if err != nil {
 		return err
 	}
