17c3215985d8cb4cb486fe3da5033fab33ec3673
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-07 22:14:47 +0300

    add docker push on pr commits and gh releases

---
+++ build/docker_push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION_REGEX="/\d(.\d)+/"
+
+IMAGE_TAG="latest"
+
+if [ $TRAVIS_TAG =~ $VERSION_REGEX ]; then
+    IMAGE_TAG=$TRAVIS_TAG
+fi;
+
+docker build -f docker/web-app/Dockerfile -t $WEB_TAG:$IMAGE_TAG
+
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_LOGIN --password-stdin;
+
+docker tag $WEB_TAG:$IMAGE_TAG $DOCKER_LOGIN/$WEB_TAG:$IMAGE_TAG;
+
+docker push $DOCKER_LOGIN/$WEB_TAG:$IMAGE_TAG;
