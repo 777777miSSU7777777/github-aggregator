76129a7f196ea7a42ee23c2cfb88c4e3a4255de5
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-09 04:19:54 +0300

    add factory for organizations

---
+++ pkg/factory/orgsfactory/orgs_factory.go
@@ -0,0 +1,23 @@
+// Package orgsfactory contains a factory for array of Organizations.
+package orgsfactory
+
+import (
+	"encoding/json"
+
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+)
+
+// New returns and array of Organizations.
+// Byte array param "orgsBytes" responsible for organizations data from organizations query.
+// If json.Unmarshal occurs any error, this will be returned.
+func New(orgsBytes []byte) (*[]entity.Organization, error) {
+	orgs := []entity.Organization{}
+
+	err := json.Unmarshal(orgsBytes, &orgs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &orgs, nil
+}
