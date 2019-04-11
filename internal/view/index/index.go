62c506eb63090100e4857f5fd9b2496e46c5a6b1
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-12 02:12:16 +0300

    ref index view and add login view

--- internal/view/index/index.go
+++ internal/view/index/index.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/777777miSSU7777777/github-aggregator/internal/security/webtokenservice"
 	"github.com/777777miSSU7777777/github-aggregator/internal/view"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/factory/profilefactory"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/log"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/query"
 )
+
+type IndexState struct {
+	Auth bool
+}
 
 // Render renders index page.
 // If user not authenticated, will be rendered form with token field and submit button.
@@ -18,39 +20,17 @@ import (
 // If occurs any error (besides cookie not found) to client will be returned "Internal server error".
 func Render(rw http.ResponseWriter, req *http.Request) {
 	tkn, err := webtokenservice.GetToken(req)
+
 	if err != nil {
-		log.Warning.Println(err)
+		log.Error.Println(err)
 	}
 
-	if tkn != "" {
-		userBytes, err := query.GetUser(tkn)
-		if err != nil {
-			log.Warning.Println(err)
-			http.Error(rw, "Internal server error", http.StatusInternalServerError)
-		}
+	err = view.GetTemplates().ExecuteTemplate(rw, "index.gohtml", IndexState{Auth: tkn != ""})
 
-		scopes, err := query.GetScopes(tkn)
-		if err != nil {
-			log.Warning.Println(err)
-			http.Error(rw, "Internal server error", http.StatusInternalServerError)
-		}
-
-		profile, err := profilefactory.New(userBytes, scopes)
-		if err != nil {
-			log.Warning.Println(err)
-			http.Error(rw, "Internal server error", http.StatusInternalServerError)
-		}
-
-		err = view.GetTemplates().ExecuteTemplate(rw, "index-authorized.gohtml", profile)
-		if err != nil {
-			log.Error.Println(err)
-			http.Error(rw, "Internal server error", http.StatusInternalServerError)
-		}
+	if err != nil {
+		log.Error.Println(err)
+		http.Error(rw, "Internal server error", http.StatusInternalServerError)
 	} else {
-		err = view.GetTemplates().ExecuteTemplate(rw, "index.gohtml", nil)
-		if err != nil {
-			log.Error.Println(err)
-			http.Error(rw, "Internal server error", http.StatusInternalServerError)
-		}
+		log.Info.Println("Index page successfuly rendered")
 	}
 }
