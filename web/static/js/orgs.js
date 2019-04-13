207458dabff8cce0d1164a195e94d72dd52ef248
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-14 00:35:48 +0300

    add organizations toggle (enable/disable)

--- web/static/js/orgs.js
+++ web/static/js/orgs.js
@@ -4,15 +4,33 @@ var orgsDropdownButton
 
 var orgsDropdownMenu;
 
+const enabledOrgClass = "list-group-item-success";
+
+const disabledOrgClass = "list-group-item-secondary"
+
 $(document).ready(() => {
     orgsDropdownButton = document.getElementById("orgs-dropdown-button");
 
     orgsDropdownMenu = document.getElementById("orgs-dropdown-menu");
 
-    $("#orgs-dropdown-button").click(() => {
+    $(document).on("click", "#orgs-dropdown-button", () => {
         orgs();
         $(orgsDropdownMenu).toggleClass("show");
-    })
+    });
+
+    $(document).on("click", ".org-item", event => {
+        const toggleOrg = $(event.target);
+
+        if ( $(toggleOrg).hasClass(disabledOrgClass) ){
+            $(toggleOrg)
+                .removeClass(disabledOrgClass)
+                .addClass(enabledOrgClass);
+        } else if ( $(toggleOrg).hasClass(enabledOrgClass)){
+            $(toggleOrg)
+                .removeClass(enabledOrgClass)
+                .addClass(disabledOrgClass);
+        }
+    });
 })
 
 function orgs(){
@@ -27,13 +45,13 @@ function renderOrgs(orgsData){
         listItem.id = "org-" + org["login"];
         
         let pElem = document.createElement("p");
-        pElem.className = "dropdown-item list-group-item list-group-item-secondary";
+        pElem.className = "dropdown-item org-item list-group-item list-group-item-secondary";
         pElem.innerHTML = org["login"];
 
         listItem.appendChild(pElem);
 
         return listItem
-    })
+    });
 
     let divOrgsList = document.getElementById("orgs-dropdown-menu");
 
