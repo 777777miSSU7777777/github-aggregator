8eae64e3cd49d9a5734b60b7d5f79c472fa18bcc
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-14 03:11:43 +0300

    add user choice save between sessins

--- web/static/js/orgs.js
+++ web/static/js/orgs.js
@@ -6,14 +6,14 @@ var orgsDropdownMenu;
 
 const enabledOrgClass = "list-group-item-success";
 
-const disabledOrgClass = "list-group-item-secondary"
+const disabledOrgClass = "list-group-item-secondary";
 
 $(document).ready(() => {
     orgsDropdownButton = document.getElementById("orgs-dropdown-button");
 
     orgsDropdownMenu = document.getElementById("orgs-dropdown-menu");
 
-    $(document).on("click", "#orgs-dropdown-button", () => {
+    $("#orgs-dropdown-button").click(() => {
         orgs();
         $(orgsDropdownMenu).toggleClass("show");
     });
@@ -21,36 +21,51 @@ $(document).ready(() => {
     $(document).on("click", ".org-item", event => {
         const toggleOrg = $(event.target);
 
+        let org = toggleOrg.text();
+
         if ( $(toggleOrg).hasClass(disabledOrgClass) ){
             $(toggleOrg)
                 .removeClass(disabledOrgClass)
                 .addClass(enabledOrgClass);
+            addOrg(org)
         } else if ( $(toggleOrg).hasClass(enabledOrgClass)){
             $(toggleOrg)
                 .removeClass(enabledOrgClass)
                 .addClass(disabledOrgClass);
+            delOrg(org);
         }
     });
-})
+});
 
 function orgs(){
     fetch("/orgs", {method: "GET"})
         .then(response => response.json())
-        .then(data => renderOrgs(data))
+        .then(data => renderOrgs(data));
 }
 
 function renderOrgs(orgsData){
+    let orgsChoice = JSON.parse(localStorage.getItem("orgs_choice"));
+
     let liOrgsList = orgsData.map(org => {
         let listItem = document.createElement("div");
         listItem.id = "org-" + org["login"];
         
         let pElem = document.createElement("p");
-        pElem.className = "dropdown-item org-item list-group-item list-group-item-secondary";
+        pElem.className = "dropdown-item org-item list-group-item";
+
+        if (org["login"] in orgsChoice){
+            $(pElem).addClass(enabledOrgClass);
+            console.log("en")
+        } else {
+            $(pElem).addClass(disabledOrgClass);
+            console.log("di")
+        }
+
         pElem.innerHTML = org["login"];
 
         listItem.appendChild(pElem);
 
-        return listItem
+        return listItem;
     });
 
     let divOrgsList = document.getElementById("orgs-dropdown-menu");
@@ -61,4 +76,24 @@ function renderOrgs(orgsData){
 
     liOrgsList.forEach(orgItem => divOrgsList.appendChild(orgItem));
 }
+
+function addOrg(org){
+    if (localStorage.getItem("orgs_choice") === undefined){
+        localStorage.setItem("orgs_choice","{}");
+    }
+
+    let orgsChoice = JSON.parse(localStorage.getItem("orgs_choice"));
+    orgsChoice[org] = org;
+    localStorage.setItem("orgs_choice", JSON.stringify(orgsChoice));
+}
+
+function delOrg(org){
+    let orgsChoice = JSON.parse(localStorage.getItem("orgs_choice"));
+    delete orgsChoice[org];
+    localStorage.setItem("orgs_choice", JSON.stringify(orgsChoice));
+}
+
+
+
+
 
