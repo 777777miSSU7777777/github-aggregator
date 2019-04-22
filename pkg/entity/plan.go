c4d63cadc5acaeb3446c0480d0e43b03da59fc4d
777777miSSU7777777 <43317471+777777miSSU7777777@users.noreply.github.com>
2019-04-22 22:04:20 +0300

    reduce entity fields and fix it's deps (#61)

--- pkg/entity/plan.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-// Plan is an entity which represents User's entity field for Github paid plan.
-type Plan struct {
-	Name          string `json:"name"`
-	Space         int    `json:"space"`
-	Collabarators int    `json:"collabarators"`
-	PrivateRepos  int    `json:"private_repos"`
-}
