b98ae7f3f23680740e42d5197031a91e8878e684
777777miSSU7777777 <43317471+777777miSSU7777777@users.noreply.github.com>
2019-04-23 10:29:07 +0300

    Revert "reduce entity fields and fix it's deps (#61)" (#62)
    This reverts commit c4d63cadc5acaeb3446c0480d0e43b03da59fc4d.

--- pkg/entity/organization.go
+++ pkg/entity/organization.go
@@ -2,7 +2,15 @@ package entity
 
 // Organization is an entity which represents body of request to "https://api.github.com/organizations" for authenticated user.
 type Organization struct {
-	Login     string `json:"login"`
-	ReposURL  string `json:"repos_url"`
-	AvatarURL string `json:"avatar_url"`
+	Login            string `json:"login"`
+	ID               int    `json:"id"`
+	NodeID           string `json:"node_id"`
+	ReposURL         string `json:"repos_url"`
+	EventsURL        string `json:"events_url"`
+	HooksURL         string `json:"hooks_url"`
+	IssuesURL        string `json:"issues_url"`
+	MembersURL       string `json:"members_url"`
+	PublicMembersURL string `json:"public_members_url"`
+	AvatarURL        string `json:"avatar_url"`
+	Description      string `json:"description"`
 }
---
+++ pkg/factory/profilefactory/profile_factory.go
@@ -0,0 +1,24 @@
+// Package profilefactory contains a factory for profile entity.
+package profilefactory
+
+import (
+	"encoding/json"
+
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+)
+
+// New returns an instance of Profile.
+// Byte array param "userBytes" is responsible for user data from user query.
+// String array param "scopes" is responsible for Github API scopes for provided token from scopes query.
+// If json.Unmarshal occurs any error, this will be returned.
+func New(userBytes []byte) (*entity.Profile, error) {
+	profile := entity.Profile{}
+
+	err := json.Unmarshal(userBytes, &profile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &profile, nil
+}
