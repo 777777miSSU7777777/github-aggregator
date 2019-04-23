daebce4178b9215806f8f2cc73f68800c388e618
777777miSSU7777777 <43317471+777777miSSU7777777@users.noreply.github.com>
2019-04-23 21:17:05 +0300

    Feature session system (#65) (#66)
    * add session entity and session service
    
    * fix session close
    
    * add session service to api

--- internal/api/logout.go
+++ internal/api/logout.go
@@ -7,11 +7,14 @@ import (
 
 	"github.com/777777miSSU7777777/github-aggregator/internal/security/tokenservice"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/log"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/session"
 )
 
 // Logout logs out user from app.
 func Logout(rw http.ResponseWriter, req *http.Request) {
 	tokenservice.DeleteToken()
+
+	session.GetSessionService().CloseSession()
 
 	log.Info.Println("Logout")
 
--- internal/api/orgs.go
+++ internal/api/orgs.go
@@ -1,35 +1,20 @@
 package api
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 
-	"github.com/777777miSSU7777777/github-aggregator/internal/security/tokenservice"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/factory/orgsfactory"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/log"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/query"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/session"
 )
 
 // Orgs returns response with current organizations info in json format.
 func Orgs(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 
-	tkn := tokenservice.GetToken()
+	orgs := session.GetSessionService().GetSession().GetUserOrgs()
 
-	orgsBytes, err := query.GetDataSource().GetOrgs(context.Background(), tkn)
-
-	if err != nil {
-		log.Error.Println(err)
-	}
-
-	orgs, err := orgsfactory.New(orgsBytes)
-
-	if err != nil {
-		log.Error.Println(err)
-	}
-
-	err = json.NewEncoder(rw).Encode(orgs)
+	err := json.NewEncoder(rw).Encode(orgs)
 
 	if err != nil {
 		log.Error.Println(err)
