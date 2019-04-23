decc78328930950dfbd589ad9fc2c2d3b429518d
777777miSSU7777777 <43317471+777777miSSU7777777@users.noreply.github.com>
2019-04-23 21:17:36 +0300

    Revert "Feature session system (#65) (#66)"
    This reverts commit daebce4178b9215806f8f2cc73f68800c388e618.

--- internal/api/orgs.go
+++ internal/api/orgs.go
@@ -1,20 +1,35 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
+	"github.com/777777miSSU7777777/github-aggregator/internal/security/tokenservice"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/factory/orgsfactory"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/log"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/session"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/query"
 )
 
 // Orgs returns response with current organizations info in json format.
 func Orgs(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 
-	orgs := session.GetSessionService().GetSession().GetUserOrgs()
+	tkn := tokenservice.GetToken()
 
-	err := json.NewEncoder(rw).Encode(orgs)
+	orgsBytes, err := query.GetDataSource().GetOrgs(context.Background(), tkn)
+
+	if err != nil {
+		log.Error.Println(err)
+	}
+
+	orgs, err := orgsfactory.New(orgsBytes)
+
+	if err != nil {
+		log.Error.Println(err)
+	}
+
+	err = json.NewEncoder(rw).Encode(orgs)
 
 	if err != nil {
 		log.Error.Println(err)
--- internal/api/profile.go
+++ internal/api/profile.go
@@ -1,20 +1,35 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
+	"github.com/777777miSSU7777777/github-aggregator/internal/security/tokenservice"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/factory/userfactory"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/log"
-	"github.com/777777miSSU7777777/github-aggregator/pkg/session"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/query"
 )
 
 // Profile returns repsonse with current profile info in json format.
 func Profile(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 
-	profile := session.GetSessionService().GetSession().GetCurrentUser()
+	tkn := tokenservice.GetToken()
 
-	err := json.NewEncoder(rw).Encode(profile)
+	userBytes, err := query.GetDataSource().GetUser(context.Background(), tkn)
+
+	if err != nil {
+		log.Error.Println(err)
+	}
+
+	profile, err := userfactory.New(userBytes)
+
+	if err != nil {
+		log.Error.Println(err)
+	}
+
+	err = json.NewEncoder(rw).Encode(profile)
 
 	if err != nil {
 		log.Error.Println(err)
