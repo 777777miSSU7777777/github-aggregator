e24d6960ae6e6ffd27715605f99010af8475d352
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-24 23:33:56 +0300

    add repos and prs queries

--- pkg/query/datasource/github_rest_api.go
+++ pkg/query/datasource/github_rest_api.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/777777miSSU7777777/github-aggregator/pkg/constants"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/http/bodyutil"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/http/headerutil"
 )
@@ -69,4 +70,57 @@ func (ds GithubRESTAPI) GetOrgs(ctx context.Context, token string) ([]byte, erro
 	}
 
 	return orgsBody, nil
+}
+
+// GetOrgsRepos returns body of request to "https://api.github.com/orgs/:org/repos" for provided Github API access token.
+// Access token should be presented as string.
+// Body is presented as byte array.
+// If http.Get or bodyutil.ReadResponseBody occurs any error, this will be returned.
+func (ds GithubRESTAPI) GetOrgsRepos(ctx context.Context, token string, orgs []entity.Organization) ([]byte, error) {
+	resultSetBytes := []byte{}
+
+	for _, org := range orgs {
+		resp, err := http.Get(org.ReposURL)
+
+		if err != nil {
+			return nil, err
+		}
+
+		reposBody, err := bodyutil.ReadResponseBody(resp)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resultSetBytes = append(resultSetBytes, reposBody...)
+	}
+
+	return resultSetBytes, nil
+}
+
+// GetOrgsPullRequests blank
+func (ds GithubRESTAPI) GetOrgsPullRequests(ctx context.Context, token string, repos []entity.Repository) ([]byte, error) {
+	resultSetBytes := []byte{}
+
+	for _, repo := range repos {
+		resp, err := http.Get(trimPullsURL(repo.PullsURL))
+
+		if err != nil {
+			return nil, err
+		}
+
+		pullsBody, err := bodyutil.ReadResponseBody(resp)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resultSetBytes = append(resultSetBytes, pullsBody...)
+	}
+
+	return resultSetBytes, nil
+}
+
+func trimPullsURL(pullsUrls string) string {
+	return strings.Trim(pullsUrls, "{/number/}")
 }
