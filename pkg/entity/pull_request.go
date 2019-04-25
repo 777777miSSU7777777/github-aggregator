02ce0f9cd3414e8b20202691362114d0d018e930
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-25 20:48:52 +0300

    fix pull request content

--- pkg/entity/pull_request.go
+++ pkg/entity/pull_request.go
@@ -4,10 +4,14 @@ type Assignee User
 type Reviewer User
 
 type PullRequest struct {
-	HTMLURL            string       `json:"html_url"`
-	State              string       `json:"state"`
-	Title              string       `json:"title"`
-	Assignees          []Assignee   `json:"assignees"`
-	RequestedReviewers []Reviewer   `json:"requested_reviewers"`
-	Repo               []Repository `json:"repo"`
+	HTMLURL            string     `json:"html_url"`
+	State              string     `json:"state"`
+	Title              string     `json:"title"`
+	Assignees          []Assignee `json:"assignees"`
+	RequestedReviewers []Reviewer `json:"requested_reviewers"`
+	Head               head       `json:"head"`
+}
+
+type head struct {
+	Repo Repository `json:"repo"`
 }
