938043cbfd5e149f749552ae110dd45e09c9f41e
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-04-26 02:50:50 +0300

    add user field to pr entity

--- pkg/entity/pull_request.go
+++ pkg/entity/pull_request.go
@@ -7,6 +7,7 @@ type PullRequest struct {
 	HTMLURL            string     `json:"html_url"`
 	State              string     `json:"state"`
 	Title              string     `json:"title"`
+	User               User       `json:"user"`
 	Assignees          []Assignee `json:"assignees"`
 	RequestedReviewers []Reviewer `json:"requested_reviewers"`
 	Head               head       `json:"head"`
