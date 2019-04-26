50e36acec3143d44f88d6c0f63c854d4bdb47341
777777miSSU7777777 <6x7missu7x7@gmail.com>
2019-04-26 15:26:55 +0300

    add tests for orgs filter

---
+++ pkg/filter/orgsfilter/orgs_filter_test.go
@@ -0,0 +1,52 @@
+package orgsfilter
+
+import (
+	"testing"
+
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilterByChoice__OrgsChoiceNotEmpty__Equal(t *testing.T) {
+	allOrgs := []entity.Organization{}
+
+	for i := 0; i < 10; i++ {
+		allOrgs = append(allOrgs, entity.Organization{Login: string(i)})
+	}
+
+	orgsChoice := []string{}
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			orgsChoice = append(orgsChoice, string(i))
+		}
+	}
+
+	testOrgs := []entity.Organization{}
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			testOrgs = append(testOrgs, entity.Organization{Login: string(i)})
+		}
+	}
+
+	filterOrgs := FilterByChoice(allOrgs, orgsChoice)
+
+	assert.Equal(t, testOrgs, filterOrgs)
+}
+
+func TestFilterByChoice__OrgsChoiceEmpty__Equal(t *testing.T) {
+	allOrgs := []entity.Organization{}
+
+	for i := 0; i < 10; i++ {
+		allOrgs = append(allOrgs, entity.Organization{Login: string(i)})
+	}
+
+	orgsChoice := []string{}
+
+	testOrgs := []entity.Organization{}
+
+	filterOrgs := FilterByChoice(allOrgs, orgsChoice)
+
+	assert.Equal(t, testOrgs, filterOrgs)
+}
