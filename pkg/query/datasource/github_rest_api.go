13c8818235bf134b417783541fcc400572bf6d94
777777miSSU7777777 <43317471+777777miSSU7777777@users.noreply.github.com>
2019-05-09 19:46:46 +0300

    Rewrite GetOrgsRepos and GetReposPullRequests to be async. (#79)
    * rewrite repos query to async impl
    
    * fix GetOrgsRepos async impl
    
    * rewrite pulls query to async impl with WaitGroup
    
    * fix pull query async impl

--- pkg/query/datasource/github_rest_api.go
+++ pkg/query/datasource/github_rest_api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
 	"github.com/777777miSSU7777777/github-aggregator/pkg/http/bodyutil"
@@ -112,22 +113,18 @@ func (ds GithubRESTAPI) GetOrgs(ctx context.Context, token string) ([]byte, erro
 func (ds GithubRESTAPI) GetOrgsRepos(ctx context.Context, token string, orgs []entity.Organization) ([][]byte, error) {
 	resultSetBytes := [][]byte{}
 
+	orgChan := make(chan entity.Organization, len(orgs))
+	reposBytesChan := make(chan []byte, 1)
+	errorsChan := make(chan error, 1)
+
 	for _, org := range orgs {
-		req, err := http.NewRequest("GET", org.ReposURL, nil)
+		orgChan <- org
 
-		if err != nil {
-			return nil, err
-		}
+		go ds.getOrgRepos(ctx, token, orgChan, reposBytesChan, errorsChan)
 
-		req.Header.Set(OAUTH2_HEADER, OAUTH2_PREFIX+token)
+		reposBody := <-reposBytesChan
 
-		resp, err := ds.client.Do(req)
-
-		if err != nil {
-			return nil, err
-		}
-
-		reposBody, err := bodyutil.ReadResponseBody(resp)
+		err := <-errorsChan
 
 		if err != nil {
 			return nil, err
@@ -139,26 +136,58 @@ func (ds GithubRESTAPI) GetOrgsRepos(ctx context.Context, token string, orgs []e
 	return resultSetBytes, nil
 }
 
+func (ds GithubRESTAPI) getOrgRepos(ctx context.Context, token string, orgChan chan entity.Organization,
+	reposBytesChan chan []byte, errorsChan chan error) {
+	org := <-orgChan
+	req, err := http.NewRequest("GET", org.ReposURL, nil)
+
+	if err != nil {
+		reposBytesChan <- nil
+		errorsChan <- err
+		return
+	}
+
+	req.Header.Set(OAUTH2_HEADER, OAUTH2_PREFIX+token)
+
+	resp, err := ds.client.Do(req)
+
+	if err != nil {
+		reposBytesChan <- nil
+		errorsChan <- err
+		return
+	}
+
+	reposBody, err := bodyutil.ReadResponseBody(resp)
+
+	if err != nil {
+		reposBytesChan <- nil
+		errorsChan <- err
+		return
+	}
+
+	reposBytesChan <- reposBody
+	errorsChan <- nil
+}
+
 // GetReposPullRequests returns body with orgs repos pulls.
 func (ds GithubRESTAPI) GetReposPullRequests(ctx context.Context, token string, repos []entity.Repository) ([][]byte, error) {
 	resultSetBytes := [][]byte{}
 
+	repoChan := make(chan entity.Repository, len(repos))
+	pullsBytesChan := make(chan []byte, 1)
+	errorsChan := make(chan error, 1)
+
+	var wg sync.WaitGroup
+	wg.Add(len(repos))
+
 	for _, repo := range repos {
-		req, err := http.NewRequest("GET", trimPullsURL(repo.PullsURL), nil)
+		repoChan <- repo
 
-		if err != nil {
-			return nil, err
-		}
+		go ds.getReposPulls(ctx, token, repoChan, pullsBytesChan, errorsChan, &wg)
 
-		req.Header.Set(OAUTH2_HEADER, OAUTH2_PREFIX+token)
+		pullsBody := <-pullsBytesChan
 
-		resp, err := ds.client.Do(req)
-
-		if err != nil {
-			return nil, err
-		}
-
-		pullsBody, err := bodyutil.ReadResponseBody(resp)
+		err := <-errorsChan
 
 		if err != nil {
 			return nil, err
@@ -167,7 +196,43 @@ func (ds GithubRESTAPI) GetReposPullRequests(ctx context.Context, token string, 
 		resultSetBytes = append(resultSetBytes, pullsBody)
 	}
 
+	wg.Wait()
+
 	return resultSetBytes, nil
+}
+
+func (ds GithubRESTAPI) getReposPulls(ctx context.Context, token string, repoChan chan entity.Repository,
+	pullsBytesChan chan []byte, errorsChan chan error, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	repo := <-repoChan
+
+	req, err := http.NewRequest("GET", trimPullsURL(repo.PullsURL), nil)
+
+	if err != nil {
+		pullsBytesChan <- nil
+		errorsChan <- err
+		return
+	}
+
+	req.Header.Set(OAUTH2_HEADER, OAUTH2_PREFIX+token)
+
+	resp, err := ds.client.Do(req)
+
+	if err != nil {
+		pullsBytesChan <- nil
+		errorsChan <- err
+	}
+
+	pullsBody, err := bodyutil.ReadResponseBody(resp)
+
+	if err != nil {
+		pullsBytesChan <- nil
+		errorsChan <- err
+	}
+
+	pullsBytesChan <- pullsBody
+	errorsChan <- nil
 }
 
 func trimPullsURL(pullsUrls string) string {
