3e1469490b0ba2b37297bdf6a838542643513244
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-05-10 19:29:56 +0300

    add rest service interface

---
+++ gokit/service/restservice/rest_service_interface.go
@@ -0,0 +1,12 @@
+package restservice
+
+import (
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+)
+
+type RestService interface {
+	CurrentUser() entity.User
+	TokenScopes() []entity.Scope
+	UserOrgs() []entity.Organization
+	FilteredPulls(string, []entity.Organization) []entity.PullRequest
+}
