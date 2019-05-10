32b7cf38389d4696bc9b8e1b5ea2074fae6b610a
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-05-10 23:09:10 +0300

    add rest service impl

---
+++ gokit/service/restservice/rest_service_impl.go
@@ -0,0 +1,92 @@
+package restservice
+
+import (
+	"context"
+
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/factory/prsfactory"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/factory/reposfactory"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/filter/orgsfilter"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/filter/prfilter"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/query"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/session"
+	"github.com/777777miSSU7777777/github-aggregator/pkg/token"
+)
+
+const (
+	// ALL filter type for pulls assigned or review requested to user.
+	ALL = "all"
+
+	// ASSIGNEE filter type for pulls assigned to user.
+	ASSIGNEE = "assignee"
+
+	// REVIEWER type for pulls review requested to user.
+	REVIEWER = "reviewer"
+)
+
+type RESTServiceImpl struct {
+}
+
+func (s RESTServiceImpl) CurrentUser() entity.User {
+	return session.GetSessionService().GetSession().GetCurrentUser()
+}
+
+func (s RESTServiceImpl) TokenScopes() ([]entity.Scope, error) {
+	tkn := token.GetTokenService().GetToken()
+	scopes, err := query.GetDataSource().GetScopes(context.Background(), tkn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return scopes, nil
+}
+
+func (s RESTServiceImpl) UserOrgs() []entity.Organization {
+	return session.GetSessionService().GetSession().GetUserOrgs()
+}
+
+func (s RESTServiceImpl) FilteredPulls(filter string, orgsChoice []string) ([]entity.PullRequest, error) {
+	orgs := session.GetSessionService().GetSession().GetUserOrgs()
+
+	orgs = orgsfilter.FilterByChoice(orgs, orgsChoice)
+
+	tkn := token.GetTokenService().GetToken()
+
+	reposBytes, err := query.GetDataSource().GetOrgsRepos(context.Background(), tkn, orgs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	repos, err := reposfactory.New(reposBytes)
+
+	if err != nil {
+		return nil, err
+	}
+
+	pullsBytes, err := query.GetDataSource().GetReposPullRequests(context.Background(), tkn, repos)
+
+	if err != nil {
+		return nil, err
+	}
+
+	pulls, err := prsfactory.New(pullsBytes)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user := session.GetSessionService().GetSession().GetCurrentUser()
+
+	switch filter {
+	case ALL:
+		pulls = prfilter.FilterByParticipation(pulls, user)
+	case ASSIGNEE:
+		pulls = prfilter.FilterByAssignee(pulls, entity.Assignee(user))
+	case REVIEWER:
+		pulls = prfilter.FilterByReviewer(pulls, entity.Reviewer(user))
+	}
+
+	return pulls, nil
+}
