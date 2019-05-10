4fb6259d0a5194c549e9b82ec53d1069697d9b64
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-05-10 19:33:59 +0300

    add responses for rest service

---
+++ gokit/response/rest_service_responses.go
@@ -0,0 +1,21 @@
+package response
+
+import (
+	"github.com/777777miSSU7777777/github-aggregator/pkg/entity"
+)
+
+type CurrentUserResponse struct {
+	CurrentUser entity.User `json:"current_user"`
+}
+
+type TokenScopesResponse struct {
+	TokenScopes []entity.Scope `json:"token_scopes"`
+}
+
+type UserOrgsResponse struct {
+	UserOrgs []entity.Organization `json:"user_orgs"`
+}
+
+type FilteredPullsResponse struct {
+	FilteredPulls entity.PullRequest `json:"filtered_pulls"`
+}
