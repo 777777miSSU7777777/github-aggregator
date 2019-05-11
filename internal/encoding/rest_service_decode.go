0bb6dd9830efdb3ed9086af230573b875a9489d8
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-05-11 03:49:48 +0300

    fix rest service decoder

--- internal/encoding/rest_service_decode.go
+++ internal/encoding/rest_service_decode.go
@@ -3,6 +3,7 @@ package encoding
 import (
 	"context"
 	"encoding/json"
+	"net/http"
 
 	"github.com/777777miSSU7777777/github-aggregator/internal/request"
 )
@@ -10,39 +11,39 @@ import (
 func decodeCurrentUser(_ context.Context, r *http.Request) (interface{}, error) {
 	var req request.CurrentUserRequest
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return nil, err
 	}
 
-	return request, nil
+	return req, nil
 }
 
 func decodeTokenScopes(_ context.Context, r *http.Request) (interface{}, error) {
 	var req request.TokenScopesRequest
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return nil, err
 	}
 
-	return request, nil
+	return req, nil
 }
 
 func decodeUserOrgs(_ context.Context, r *http.Request) (interface{}, error) {
 	var req request.UserOrgsRequest
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return nil, err
 	}
 
-	return request, nil
+	return req, nil
 }
 
 func decodeFilteredPulls(_ context.Context, r *http.Request) (interface{}, error) {
 	var req request.FilteredPullsReq
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return nil, err
 	}
 
-	return request, nil
+	return req, nil
 }
